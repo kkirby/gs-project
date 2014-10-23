8dc5f165990611b7267a2775eef0d1b69cdaafa9
Kyle Kirby <kylekirby@optimalconnection.net>
2014-10-23 12:28:16 -0500

    Updated windows ruby for SASS

--- lib/ruby_ship/bin/shipyard/win_ruby/bin/sass.bat
+++ lib/ruby_ship/bin/shipyard/win_ruby/bin/sass.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "Z:/Downloads/ruby_ship-master/bin/shipyard/win_ruby/bin/sass" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ruby.exe" "C:/Users/kkirby/HubbleMobile/node_modules/grunt-gsScriptProject/node_modules/GsScriptProject/lib/ruby_ship/bin/shipyard/win_ruby/bin/sass" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ruby.exe" "%~dpn0" %*
