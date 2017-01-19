e442bef107b945fe1f30c28de8ed4b9fa62476ca
Matt Schrader <mschrader@gmrmarketing.com>
2017-01-19 13:49:57 -0600

    Move server functionality to a server class and convert cli to use that, add server to compiler

--- server/index.js
+++ server/index.js
@@ -1,51 +1,9 @@
-var fs = require('fs'),
-		path = require('path'),
-		express = require('express'),
-		app = express(),
-		argv = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2))
+		Server = require('./Server.js');
 
 var staticRelativeDir = argv['_'][0];
-if (!staticRelativeDir) {
-	throw new Error("First parameter must exist and be a relative path to where the static content is");
-}
+var port = argv.port || argv.p;
+var corsPort = argv['cors-port'] || argv.c;
 
-// First treat it as an absolute path
-var staticDir = path.resolve(staticRelativeDir);
-if(!fs.existsSync(path.resolve(staticDir, "index.html"))) {
-	staticDir = path.resolve(__dirname, staticRelativeDir); //No?  Change to checking relative
-}
-
-// Then relative
-staticDir = path.resolve(__dirname, staticRelativeDir);
-if(!fs.existsSync(path.resolve(staticDir, "index.html"))) {
-	staticDir = path.resolve(staticDir, "build"); //No? Change to the build directory and see if it's there
-}
-
-if(!fs.existsSync(path.resolve(staticDir, "index.html"))) {
-	throw new Error("Could not find index file to serve");
-}
-
-var defaultPort = 8080;
-var port = argv.port || argv.p || defaultPort;
-console.dir(argv);
-console.log(port);
-if (!argv.port && !argv.p) {
-	console.log("Port not specified, defaulting to " + defaultPort);
-}
-
-app.use(express.static(staticDir))
-app.listen(port, function () {
-	console.log('App listening on port ' + port + '!');
-});
-
-var host = '0.0.0.0';
-var corsPort = argv['cors-port'];
-if (corsPort) {
-	var cors_proxy = require('cors-anywhere');
-	cors_proxy.createServer({
-			originWhitelist: [], // Allow all origins
-			redirectSameOrigin: true
-		}).listen(corsPort, host, function() {
-		console.log('Running CORS Anywhere on ' + host + ':' + corsPort);
-	});
-}
+var server = new Server(staticRelativeDir, port, corsPort);
+server.start()
